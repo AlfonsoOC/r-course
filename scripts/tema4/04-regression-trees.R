5cf86296bd9bb345258b3ed0e7791dd05e5194e4
AlfonsoOC <80928378+AlfonsoOC@users.noreply.github.com>
2022-02-07 19:16:46 -0500

    auuuu

--- scripts/tema4/04-regression-trees.R
+++ scripts/tema4/04-regression-trees.R
@@ -1,75 +1,99 @@
 #Arboles de regression
 
-install.packages("rpart.plot")
+#install.packages("rpart.plot")
 
 library(rpart)
 library(rpart.plot)
 library(caret)
 
-bh <- read.csv("../../data/tema4/BostonHousing.csv")
-#La variable que voy a predecir es MEDV "mediana de habitabilidad"
+bh <- read.csv("GitHub/r-course/data/tema4/BostonHousing.csv")
+
+#La variable que voy a predecir es MEDV "variable de habitabilidad"
+#datos de entrenamiento tomo el 70 % de los datos
 t.id <- createDataPartition(bh$MEDV, p = .7, list = F)
 
+# aqui realizo el arbol de regresion con "rpart"
+#intentando predecir MEDV en funcion de todas las demascolumnas
 bfit <- rpart(MEDV ~., data = bh[t.id,])
 bfit
 
 par(mfrow=c(1,1))
 
+# prp nos permite reprsentar el arbol de manera rafica
 prp(bfit, type = 2, nn=T,
     fallen.leaves = T, faclen = 4,
     varlen = 8, shadow.col = "gray")
+#en la grafica podemos ver que la computadora utiliza para presecir solo algunas variables, aqui usa "LSTAT" varias veces RM y DIS
 
+#La cptable nos muestra datos comprensibles de los nodos de los arboles de regresion
+#CP se refiere al factor de complejidad del arbol
+#nspli = numero de divisiones
+#error de clasificacion total con raiz cuadrada apartir del nodo raiz
+#error de validacion cruzada promedio
+#desviacion estandar de la validacion cruzada del mejor arbol
 bfit$cptable
 
+# plotcp nos muestra que tan buenos son los arboles de distintos tamanios contra el valor del error relativo
+#con esto se puede decidir cual es el mejor CP para estos datos
+#con estos datos nosotros en este caso cpodriamos cortar el arbor a CP= 5 
 plotcp(bfit)
 
+#aqui utilizamos el CP del 6 para podar el arbol 
+bfitpruned <- prune(bfit, cp = 0.01499132 )
 
-bfitpruned <- prune(bfit, cp = 0.02343779)
-
-
+#volvemos a hacer el arbol con este CP
 prp(bfitpruned, type = 2, nn=T,
     fallen.leaves = T, faclen = 4,
     varlen = 8, shadow.col = "gray")
 
-
+#aqui aislamos los datos del conjunto de prediccion y los comparamos con las predicciones echas por nuestro arbol original
 preds <- predict(bfit, bh[t.id,])
-sqrt(mean((preds - bh[t.id,]$MEDV)^2))
+sqrt(mean((preds - bh[t.id,]$MEDV)^2)) # este es el error 
 
 preds <- predict(bfit, bh[-t.id, ])
 sqrt(mean((preds - bh[-t.id,]$MEDV)^2))
 
+#aqui aislamos los datos del conjunto de prediccion y los comparamos con las predicciones echas por nuestro arbol recortado
 preds <- predict(bfitpruned, bh[t.id,])
 sqrt(mean((preds - bh[t.id,]$MEDV)^2))
 
 preds <- predict(bfitpruned, bh[-t.id, ])
 sqrt(mean((preds - bh[-t.id,]$MEDV)^2))
 
+#es buena idea cortar los arboles ya que te da arboles mas sencillos ademas de que puedes prevenir overfiting
 
 
 
 
 #predictores categóricos
 
-ed <- read.csv("../data/tema4/education.csv")
-ed$region <- factor(ed$region)
+ed <- read.csv("GitHub/r-course/data/tema4/education.csv")
+ed$region <- factor(ed$region) #Region es un factor 
 t.id <- createDataPartition(ed$expense, p = 0.7, list = F)
-fit <- rpart(expense ~ region+urban+income+under18, data = ed[t.id,])
+fit <- rpart(expense ~ region+urban+income+under18, data = ed[t.id,]) #predecir expense en funcion de region, urgan, income y under18
 prp(fit, type = 2, nn=T, fallen.leaves = T, 
     faclen=4, varlen=8, shadow.col = "gray")
 
-#Bagging 
-install.packages("ipred")
-library(ipred)
-bagging.fit <- bagging(MEDV~., data=bh[t.id, ])
-prediction.t <- predict(bagging.fit, bh[t.id,])
-sqrt(mean((prediction.t-bh[t.id,]$MEDV)^2))
+#METODOS DE ENSAMBLAJE
 
-prediction.v <- predict(bagging.fit, bh[-t.id,])
+#Bagging 
+#combina de manera conjunta las predicciones de diferentes arboles para dar un mejor resultado
+#es bueno para metodos con alta varianza 
+#install.packages("ipred")
+library(ipred)
+bagging.fit <- bagging(MEDV~., data=bh[t.id, ]) #usando los mismos treding id, prediciendo MEDV en funcion de todo lo demas
+prediction.t <- predict(bagging.fit, bh[t.id,]) #prediccion sobre el conjunto de entrenamiento
+sqrt(mean((prediction.t-bh[t.id,]$MEDV)^2)) #calcular el error cuadratico medio
+
+prediction.v <- predict(bagging.fit, bh[-t.id,]) # prediccion en conjunto de validacion 
 sqrt(mean((prediction.v-bh[-t.id,]$MEDV)^2))
 
+
 #Boosting
-install.packages("gbm")
+#gradient boosting machines (gbm) que es el paquete que usaremos
+#install.packages("gbm")
 library(gbm)
 
-gbmfit <- gbm(MEDV~., data = bh, distribution = "gaussian")
-prediction.t <- predict(gbmfit, bh)
+gbmfit <- gbm(MEDV~., data = bh[t.id,], distribution = "gaussian") # predecir MEDV en funcion de las demas variables usando la distribucion gausiana
+prediction.t <- predict(gbmfit, bh[t.id,])
+sqrt(mean((prediction.t-bh[t.id,]$MEDV)^2))
--- scripts/tema4/05-random-forest.R
+++ scripts/tema4/05-random-forest.R
@@ -1,23 +1,25 @@
+#Random forest para predecir el valor promedio del data set de las casas de boston
+
 library(randomForest)
 library(caret)
-bh <- read.csv("../data/tema4/BostonHousing.csv")
+bh <- read.csv("GitHub/r-course/data/tema4/BostonHousing.csv")
 
 set.seed(2018)
-t.id <- createDataPartition(bh$MEDV, p = 0.7, list = F)
+t.id <- createDataPartition(bh$MEDV, p = 0.7, list = F) #70% para entrenar
 
-mod <- randomForest(x = bh[t.id, 1:13], y = bh[t.id, 14],
-                    ntree = 1000, 
-                    xtest = bh[-t.id, 1:13], ytest = bh[-t.id, 14],
-                    importance = T, keep.forest = T)
+mod <- randomForest(x = bh[t.id, 1:13], y = bh[t.id, 14], #variables independientes x, variable dependiente es y (la que quiero estimar)
+                    ntree = 1000, #numero de arboles que quiero que haga
+                    xtest = bh[-t.id, 1:13], ytest = bh[-t.id, 14], #conjunto independiente de testing xtest, y ytest es el dependiente  
+                    importance = T, keep.forest = T)  #importance =T si quieres computar las puntuaciones de todas las variables predictoras
 
-mod
+mod #aqui se imprime por consola el modelo
 
-mod$importance
+mod$importance #la variable importance del modelo, como su nombre nos dice nos muestra que variables son mas importantes para predecir el modelo
 
 
-plot(bh[t.id,]$MEDV, predict(mod, newdata = bh[t.id,]),
+plot(bh[t.id,]$MEDV, predict(mod, newdata = bh[t.id,]), 
      xlab = "Actual", ylab = "Predichos")
-abline(0,1)
+abline(0,1) #el resultado es bastante bueno y se puede ver mostrando una linea que pasa por 0 y de pendiente 1
 
 
 plot(bh[-t.id,]$MEDV, predict(mod, newdata = bh[-t.id,]),
@@ -25,5 +27,5 @@ plot(bh[-t.id,]$MEDV, predict(mod, newdata = bh[-t.id,]),
 abline(0,1)
 
 #mtry = m/3, donde m = # de predictores
-#nodesize = 5
+#nodesize = 5 - por defecto un nodo terminal necesita tener al menos 5 elementos (esto se puede cambiar)
 #maxnodes 
